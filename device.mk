11be844f5ab8ff585a52d4e739e7ac1d81088c5c
COSMIC <rayavanindra@gmail.com>
2024-06-26 13:52:50 +0530

    miatoll: Switch to QTI Thermal AIDL HAL
    Change-Id: I42f90059c8e2fd84885c7f3ddf9381ee7c9a9f3a

--- device.mk
+++ device.mk
@@ -305,11 +305,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.pixel \
-    thermal_symlinks
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal-service.qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-curtana.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-curtana.conf \
