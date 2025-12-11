0464a343f85652c027a535c9287f81b91682e02c
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    miatoll: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -76,6 +76,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
