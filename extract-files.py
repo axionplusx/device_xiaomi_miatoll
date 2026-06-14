a3b408c0fba51211a920007410ee7abdb1967c3f
Leviscape <leviscape.xyz@gmail.com>
2026-06-14 18:19:10 +0000

    miatoll: Workaround QCOM RIL doxlat override with blob fixup
    Change-Id: I722d68562d0fd557dfc2728e5426962a38ca1e14

--- extract-files.py
+++ extract-files.py
@@ -80,6 +80,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/libdpps.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/lib64/libril-qc-hal-qmi.so': blob_fixup()
+        .binary_regex_replace(b'persist.vendor.net.doxlat', b'ro.vendor.net.doxlat\x00\x00\x00\x00\x00'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
